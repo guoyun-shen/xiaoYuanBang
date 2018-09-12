2564a9d9093d166b64a5537906996160b5de3757
shengy <shengy@gomeholdings.com>
2018-09-12 18:07:52 +0800

    asdasd

---
+++ src/main/java/com/tantan/TantanApplication.java
@@ -0,0 +1,12 @@
+package com.tantan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TantanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TantanApplication.class, args);
+	}
+}
